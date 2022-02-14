e42091944af8d26c68e6b8538f0b6e7bc697d8be
Jeff Cameron <cameron.jp@gmail.com>
2022-02-14 02:41:09 -0500

    Started implementing the UCI protocol.

--- uci.cc
+++ uci.cc
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(int argc, char **argv) {
-  cout << "Hello world! " << GIT_VERSION << endl;
+  string line;
+  while (getline(cin, line)) {
+    if (line == "uci") {
+      cout << "id name jeffchess " << GIT_VERSION << endl;
+      cout << "id author Jeff Cameron jcameron.ca" << endl;
+    } else if (line == "quit") {
+      break;
+    }
+  }
   return 0;
 }
