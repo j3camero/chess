18a4941ea0a22d1bfae951d8fc4cc117ef988328
Jeff Cameron <cameron.jp@gmail.com>
2022-02-20 14:59:54 -0500

    More unit test.

--- fen-test.cc
+++ fen-test.cc
@@ -11,3 +11,13 @@ TEST_CASE("Read default start position", "[FEN]") {
   REQUIRE(b.squares[7][0] == WhiteRook);
   REQUIRE(b.squares[7][7] == WhiteRook);
 }
+
+TEST_CASE("Read position with only one piece", "[FEN]") {
+  const string fen = "8/8/8/2k5/8/8/8/8 w KQkq - 0 1";
+  Board b = FenToBoard(fen);
+  REQUIRE(b.squares[3][2] == BlackKing);
+  REQUIRE(b.squares[3][3] == Empty);
+  REQUIRE(b.squares[3][1] == Empty);
+  REQUIRE(b.squares[2][2] == Empty);
+  REQUIRE(b.squares[4][2] == Empty);
+}
