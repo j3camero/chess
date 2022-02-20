d2fe67ccdea4bc3684f5f1de623f9ee29c1e8457
Jeff Cameron <cameron.jp@gmail.com>
2022-02-20 13:15:25 -0500

    Added more FEN reading logic but still incomplete.

--- board.cc
+++ board.cc
@@ -13,5 +13,18 @@ Board::Board(const string& fen) {
 
 void Board::SetupFen(const string& fen) {
   vector<string> tokens = StringUtil::Split(fen);
+  if (tokens.size() != 6) {
+    throw "Invalid FEN string.";
+  }
+  const string piecePlacement = tokens[0];
+  const string activeColorString = tokens[1];
+  const string castleString = tokens[2];
+  const string enPassantTargetString = tokens[3];
+  const string halfmoveClockString = tokens[4];
+  const string fullMoveNumberString = tokens[5];
+  const vector<string> ranks = StringUtil::Split(piecePlacement, '/');
+  if (ranks.size() != 8) {
+    throw "Invalid FEN string.";
+  }
   squares[0][0] = BlackRook;
 }
