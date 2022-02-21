2d0fbc410ada8724eefeb883547fd9bbd1e77988
Jeff Cameron <cameron.jp@gmail.com>
2022-02-21 13:06:46 -0500

    A few more unit tests to be safe.

--- fen-test.cc
+++ fen-test.cc
@@ -8,8 +8,14 @@ TEST_CASE("Read default start position", "[FEN]") {
   const string fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
   Board b = FenToBoard(fen);
   REQUIRE(b.squares[0][0] == BlackRook);
+  REQUIRE(b.squares[0][4] == BlackKing);
   REQUIRE(b.squares[0][7] == BlackRook);
+  REQUIRE(b.squares[1][3] == BlackPawn);
+  REQUIRE(b.squares[2][5] == Empty);
+  REQUIRE(b.squares[5][1] == Empty);
+  REQUIRE(b.squares[6][2] == WhitePawn);
   REQUIRE(b.squares[7][0] == WhiteRook);
+  REQUIRE(b.squares[7][4] == WhiteKing);
   REQUIRE(b.squares[7][7] == WhiteRook);
 }
 
