46171f7773f85faa47ad97f9b133889c44bd0ae7
Jeff Cameron <cameron.jp@gmail.com>
2023-08-22 01:09:08 +0000

    Unit tests for castle moves.

--- movegen-test.cc
+++ movegen-test.cc
@@ -3,7 +3,18 @@
 #include "fen.h"
 #include "move.h"
 #include "movegen.h"
+#include "piece-moves.h"
 #include "std.h"
+
+// Helper function for finding a move in a list of moves.
+bool HasMove(const vector<Move>& v, const Move& m) {
+  for (const Move& x : v) {
+    if (x == m) {
+      return true;
+    }
+  }
+  return false;
+}
 
 TEST_CASE("Movegen: opening position", "[Movegen]") {
   const string fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
@@ -17,4 +28,106 @@ TEST_CASE("Movegen: opening position", "[Movegen]") {
                                     "f2f3", "f2f4", "g1f3", "g1h3",
                                     "g2g3", "g2g4", "h2h3", "h2h4" };
   REQUIRE(actual == expected);
+}
+
+TEST_CASE("Movegen: white castle with all options open", "[Movegen]") {
+  const string fen = "r3k2r/8/8/8/8/8/8/R3K2R w KQkq - 0 1";
+  const Board b = FenToBoard(fen);
+  const vector<Move> moves = GeneratePseudoLegalMoves(b);
+  REQUIRE(moves.size() == 26);
+  REQUIRE(HasMove(moves, whiteKingCastle));
+  REQUIRE(HasMove(moves, whiteQueenCastle));
+  REQUIRE_FALSE(HasMove(moves, blackKingCastle));
+  REQUIRE_FALSE(HasMove(moves, blackQueenCastle));
+}
+
+TEST_CASE("Movegen: black castle with all options open", "[Movegen]") {
+  const string fen = "r3k2r/8/8/8/8/8/8/R3K2R b KQkq - 0 1";
+  const Board b = FenToBoard(fen);
+  const vector<Move> moves = GeneratePseudoLegalMoves(b);
+  REQUIRE(moves.size() == 26);
+  REQUIRE(HasMove(moves, blackKingCastle));
+  REQUIRE(HasMove(moves, blackQueenCastle));
+  REQUIRE_FALSE(HasMove(moves, whiteKingCastle));
+  REQUIRE_FALSE(HasMove(moves, whiteQueenCastle));
+}
+
+TEST_CASE("Movegen: white castle blocked by flags", "[Movegen]") {
+  const string fen = "r3k2r/8/8/8/8/8/8/R3K2R w - - 0 1";
+  const Board b = FenToBoard(fen);
+  const vector<Move> moves = GeneratePseudoLegalMoves(b);
+  REQUIRE_FALSE(HasMove(moves, whiteKingCastle));
+  REQUIRE_FALSE(HasMove(moves, whiteQueenCastle));
+}
+
+TEST_CASE("Movegen: black castle blocked by flags", "[Movegen]") {
+  const string fen = "r3k2r/8/8/8/8/8/8/R3K2R b - - 0 1";
+  const Board b = FenToBoard(fen);
+  const vector<Move> moves = GeneratePseudoLegalMoves(b);
+  REQUIRE_FALSE(HasMove(moves, blackKingCastle));
+  REQUIRE_FALSE(HasMove(moves, blackQueenCastle));
+}
+
+TEST_CASE("Movegen: white castle differing flags", "[Movegen]") {
+  const string fen = "r3k2r/8/8/8/8/8/8/R3K2R w Q - 0 1";
+  const Board b = FenToBoard(fen);
+  const vector<Move> moves = GeneratePseudoLegalMoves(b);
+  REQUIRE_FALSE(HasMove(moves, whiteKingCastle));
+  REQUIRE(HasMove(moves, whiteQueenCastle));
+}
+
+TEST_CASE("Movegen: black castle differing flags", "[Movegen]") {
+  const string fen = "r3k2r/8/8/8/8/8/8/R3K2R b k - 0 1";
+  const Board b = FenToBoard(fen);
+  const vector<Move> moves = GeneratePseudoLegalMoves(b);
+  REQUIRE(HasMove(moves, blackKingCastle));
+  REQUIRE_FALSE(HasMove(moves, blackQueenCastle));
+}
+
+TEST_CASE("Movegen: white castle blocked by check", "[Movegen]") {
+  const string fen = "r3k2r/8/8/8/8/8/3p4/R3K2R w KQkq - 0 1";
+  const Board b = FenToBoard(fen);
+  const vector<Move> moves = GeneratePseudoLegalMoves(b);
+  REQUIRE_FALSE(HasMove(moves, whiteKingCastle));
+  REQUIRE_FALSE(HasMove(moves, whiteQueenCastle));
+}
+
+TEST_CASE("Movegen: white king castle blocked by attack", "[Movegen]") {
+  const string fen = "r3k2r/8/8/8/8/3b4/8/R3K2R w KQkq - 0 1";
+  const Board b = FenToBoard(fen);
+  const vector<Move> moves = GeneratePseudoLegalMoves(b);
+  REQUIRE_FALSE(HasMove(moves, whiteKingCastle));
+  REQUIRE(HasMove(moves, whiteQueenCastle));
+}
+
+TEST_CASE("Movegen: white queen castle blocked by attack", "[Movegen]") {
+  const string fen = "r3k2r/8/8/8/8/2n5/8/R3K2R w KQkq - 0 1";
+  const Board b = FenToBoard(fen);
+  const vector<Move> moves = GeneratePseudoLegalMoves(b);
+  REQUIRE(HasMove(moves, whiteKingCastle));
+  REQUIRE_FALSE(HasMove(moves, whiteQueenCastle));
+}
+
+TEST_CASE("Movegen: black castle blocked by check", "[Movegen]") {
+  const string fen = "r3k2r/8/8/8/Q7/8/8/R3K2R b KQkq - 0 1";
+  const Board b = FenToBoard(fen);
+  const vector<Move> moves = GeneratePseudoLegalMoves(b);
+  REQUIRE_FALSE(HasMove(moves, blackKingCastle));
+  REQUIRE_FALSE(HasMove(moves, blackQueenCastle));
+}
+
+TEST_CASE("Movegen: black king castle blocked by attack", "[Movegen]") {
+  const string fen = "r3k2r/8/6N1/8/8/8/8/R3K2R b KQkq - 0 1";
+  const Board b = FenToBoard(fen);
+  const vector<Move> moves = GeneratePseudoLegalMoves(b);
+  REQUIRE_FALSE(HasMove(moves, blackKingCastle));
+  REQUIRE(HasMove(moves, blackQueenCastle));
+}
+
+TEST_CASE("Movegen: black queen castle blocked by attack", "[Movegen]") {
+  const string fen = "r3k2r/3R4/8/8/8/8/8/4K3 b KQkq - 0 1";
+  const Board b = FenToBoard(fen);
+  const vector<Move> moves = GeneratePseudoLegalMoves(b);
+  REQUIRE(HasMove(moves, blackKingCastle));
+  REQUIRE_FALSE(HasMove(moves, blackQueenCastle));
 }
