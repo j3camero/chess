67cedd4129a58f128de8c1ff0c23b1ee67a0e9c3
Jeff Cameron <cameron.jp@gmail.com>
2025-05-14 16:10:46 -0400

    Move attack unit tests.

--- test.cc
+++ test.cc
@@ -1,10 +1,12 @@
 #include "assert.h"
+#include "attack.h"
 #include "check.h"
 #include "std.h"
 
 int main() {
   SetAssertCount(0);
   TestAssert();
+  TestAttack();
   TestCheck();
   cout << "All " << GetAssertCount() << " tests pass :)" << endl;
   return 0;
